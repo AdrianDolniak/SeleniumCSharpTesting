edfe56fded838fbe3cb1b22d7f933aeb2c77cf95
Adrian Dolniak <adolniak@objectivity.co.uk>
2021-04-12 13:00:46 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Selenium - Test Automation for Web Applications
+
+### Testowanie aplikacji internetowych z wykorzystaniem Selenium Webdriver z C# 
+
+---
+[Documentation](https://www.seleniumhq.org/docs/01_introducing_selenium.jsp)\
+[Download](https://www.seleniumhq.org/download/)
---
+++ tests/objectivity_page.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+
+
+namespace BasicSeleniumIntroduction.tests
+{
+    [TestFixture]
+    class SeleniumTesting
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver("D:\\Testing\\Visual Studio C # Testing\\");
+            driver.Url = "https://www.objectivity.co.uk/";
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Home_page_title()
+        {
+            string myTitle = driver.Title;
+            Assert.AreEqual(myTitle, "Software Development Company | Bespoke Software UK | Objectivity");
+            Thread.Sleep(10000);
+        }
+        [Test]
+        public void Join_us()
+        {
+            IWebElement joinUs = driver.FindElement(By.CssSelector("#navbarToggler > ul > li:nth-child(8)"));
+            joinUs.Click();
+            Thread.Sleep(10000);
+        }
+    }
+}
